486dd94a3e465b20f83a78cfbafeba81c4dc44d6
Reja <rezanurrochmat@gmail.com>
2021-12-27 20:29:06 +0700

    Add junit and mockito

---
+++ src/test/kotlin/com/kotlin/crud/presenter/MataPelajaranPresenterTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.crud.presenter
+
+
+import com.kotlin.crud.entity.MataPelajaran
+import org.junit.Test
+
+internal class MataPelajaranPresenterTests {
+
+    @Test
+    fun getAllEducationSubjects() {
+        println("Test case for get all education subjects")
+    }
+}
